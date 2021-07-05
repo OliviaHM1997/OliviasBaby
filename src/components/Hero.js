ce76d9058d8c68af99db47a54bae0c66aa1f2c80
OliviaHM1997 <oliviahillmathieson@gmail.com>
2021-07-05 15:35:04 +0100

    image and title change

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,8 +4,8 @@ import logo from "../assets/logo.svg";
 
 const Hero = () => (
   <div className="text-center hero my-5">
-    <img className="mb-3 app-logo" src={logo} alt="React logo" width="120" />
-    <h1 className="mb-4">React.js Sample Project</h1>
+    <img className="mb-3 app-logo" src="https://static.standard.co.uk/s3fs-public/thumbnails/image/2020/06/02/11/puppies-kittens.jpg" alt="Puppies and Kittens"  />
+    <h1 className="mb-4">React.js Olivia's Baby</h1>
 
     <p className="lead">
       This is a sample application that demonstrates an authentication flow for
